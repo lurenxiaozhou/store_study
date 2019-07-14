35f3b5937f977929c79631d10d59dadb37d5a907
lurenxiaozhou <1063828967@qq.com>
2019-07-14 16:42:51 +0800

    购物车-查询

--- demo_store/demo_store/apps/carts/views.py
+++ demo_store/demo_store/apps/carts/views.py
@@ -4,6 +4,7 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 import base64,pickle
 
+from goods.models import SKU
 from . import serializers,constants
 
 # Create your views here.
@@ -79,5 +80,53 @@ class CartView(GenericAPIView):
             response.set_cookie('cart',cart_cookie,max_age=constants.CART_COOKIE_EXPIRES)
 
             return response
+
+
+    def get(self,request):
+        """查询购物车"""
+        # 判断用户登录状态
+        try:
+            user = request.user
+        except Exception:
+            user = None
+
+        # 查询
+        if user and user.is_authenticated:
+            # 如果用户已经登录，从redis中查询
+            redis_conn = get_redis_connection('cart')
+            redis_cart = redis_conn.hgetall('cart_%s'%user.id)
+            redis_cart_selected = redis_conn.smembers('cart_selected_%s'%user.id)
+
+            # 遍历 redis_cart ，形成redis_cart
+            cart_dict = {}
+            for sku_id,count in redis_cart.items():
+                cart_dict[int(sku_id)] = {
+                    'count':count,
+                    'selected':sku_id in redis_cart_selected
+                }
+        else:
+            # 如果用户未登录，从cookie中查询
+            cookie_cart = request.COOKIES.get('cart')
+
+            if cookie_cart:
+                # 表示cookie中有购物车数据
+                cart_dict = pickle.loads(base64.b64decode(cookie_cart.encode()))
+            else:
+                # 表示没有
+                cart_dict = {}
+
+        # 查询数据库
+        sku_id_list = cart_dict.keys()
+        sku_obj_list = SKU.objects.filter(id__in=sku_id_list)
+
+        # 遍历sku_obj_list 向sku对象中添加count和selected属性
+        for sku in sku_obj_list:
+            sku.count = cart_dict[sku.id]['count']
+            sku.selected = cart_dict[sku.id]['selected']
+
+        # 序列化返回
+        serializer = serializers.CartSKUSerializer(sku_obj_list,many=True)
+        return Response(serializer.data)
+
 
 
